a5768f726ed51fb70f40cd44855f49065de6809f
Sarge <mdodge@pivotal.io>
2016-12-18 12:08:07 -0800

    Add by-roll probabilities.

---
+++ bacon/src/io/pivotal/bacon/MapQueue.java
@@ -0,0 +1,7 @@
+package io.pivotal.bacon;
+
+/**
+ * Created by mdodge on 15/12/2016.
+ */
+public class MapQueue {
+}
--- fudge_prob/fudge_prob.py
+++ fudge_prob/fudge_prob.py
@@ -4,33 +4,65 @@ import argparse
 import collections
 import os
 
-counts = {0 : 0}
+by_roll = {}
 
-def addScores(current, remaining):
-    if remaining <= 1:
-        #print 'current=%d remaining=%d' % (current, remaining)
-        counts[current - 1] = counts.setdefault(current - 1, 0) + 1
-        counts[current] = counts.setdefault(current, 0) + 1
-        counts[current + 1] = counts.setdefault(current + 1, 0) + 1
+def make_key(dice):
+    key = ''
+    for die in sorted(dice):
+        key += die
+    return key
+
+def score_key(key):
+    score = 0
+    dice = list(str(key))
+    for die in dice:
+        if die == '+':
+            score += 1
+        elif die == '-':
+            score -= 1
+    return score
+
+def recurse(target, current):
+    if target <= len(current):
+        key = make_key(current)
+        by_roll[key] = by_roll.setdefault(key, 0) + 1
     else:
-        addScores(current - 1, remaining - 1)
-        addScores(current, remaining - 1)
-        addScores(current + 1, remaining - 1)
+        plus = list(current)
+        plus.append('+')
+        recurse(target, plus)
+        zero = list(current)
+        zero.append('0')
+        recurse(target, zero)
+        minus = list(current)
+        minus.append('-')
+        recurse(target, minus)
 
-parser = argparse.ArgumentParser(description='Build the native client.')
+def print_probabilities(counts):
+    total = 0
+    for key in counts.iterkeys():
+        total += counts[key]
+
+    sortedCounts = collections.OrderedDict(sorted(counts.items()))
+    for key in sortedCounts.iterkeys():
+        print '%s: %d/%d (%f)' % (str(key).rjust(5, ' '), sortedCounts[key], total, ((1. * sortedCounts[key]) / total))
+
+parser = argparse.ArgumentParser(description='Calculate probabilities of fudge dice.')
 parser.add_argument('number', type=int, default=1, help='number of fudge dice')
 args = parser.parse_args()
 
-addScores(0, args.number)
+recurse(args.number, [])
 
-total = 0
-for score in counts.iterkeys():
-    total += counts[score]
+print 'By roll:'
+print_probabilities(by_roll)
 
-sortedCounts = collections.OrderedDict(sorted(counts.items()))
-for score in sortedCounts.iterkeys():
-    if 0 <= score:
-        padding = ' '
-    else:
-        padding = ''
-    print '%s%0d: %f' % (padding, score, ((1. * sortedCounts[score]) / total))
+print ''
+print 'By score:'
+by_score = {}
+for roll in by_roll.iterkeys():
+    roll = str(roll)
+    score = score_key(roll)
+    # print '%s -> %d' % (roll, score)
+    by_score[score] = by_score.setdefault(score, 0) + by_roll[roll]
+    #print 'by_score[%s]=%d' % (score, by_score[score])
+
+print_probabilities(by_score)
