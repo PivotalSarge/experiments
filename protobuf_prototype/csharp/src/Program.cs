4de3c6b50f226092a793f8299cd614f1125f37b9
Sarge <mdodge@pivotal.io>
2016-12-27 09:46:47 -0800

    Add stubs of C# client.

---
+++ protobuf_prototype/csharp/src/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace protobuf_prototype_csharp
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			Message.PutRequest putRequest = new Message.PutRequest();
+		}
+	}
+}
